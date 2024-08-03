3fae5dd19ed07671b7b7da656fb03f3100412f44
MoRanGu <1126146235@qq.com>
2024-08-03 08:55:57 +0800

    feat(user):  个人中心页面布局

--- README.md
+++ README.md
@@ -518,3 +518,129 @@ static目录下的文件，在app第一次启动时，解压到了app的外部�
 </style>
 ```
 ![](README_files/6.jpg)
+
+### 2.7.个人中心页面布局
+```vue
+<template>
+  <view class="userLayout">
+    <view class="userInfo">
+      <view class="avatar">
+        <image src="../../static/images/xxmLogo.png" mode=""></image>
+      </view>
+      <view class="ip">100.100.100.011</view>
+      <view class="address">来自于：山东</view>
+
+    </view>
+    <view class="section">
+      <view class="list">
+        <view class="row" v-for="itme in 3">
+          <view class="left">
+            <uni-icons type="download-filled" size="20" color="#28b389"></uni-icons>
+            <view class="text">我的下载</view>
+          </view>
+          <view class="right">
+            <view class="text">66</view>
+            <uni-icons type="right" size="15" color="#aaa"></uni-icons>
+          </view>
+        </view>
+
+      </view>
+    </view>
+    <view class="section">
+      <view class="list">
+        <view class="row" v-for="itme in 2">
+          <view class="left">
+            <uni-icons type="download-filled" size="20" color="#28b389"></uni-icons>
+            <view class="text">我的下载</view>
+          </view>
+          <view class="right">
+            <view class="text">66</view>
+            <uni-icons type="right" size="15" color="#aaa"></uni-icons>
+          </view>
+        </view>
+
+      </view>
+    </view>
+  </view>
+</template>
+
+<script setup>
+  import {
+    ref
+  } from "vue";
+</script>
+
+<style lang="scss" scoped>
+  .userLayout {
+    .userInfo {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 50rpx 0;
+
+      .avatar {
+        width: 160rpx;
+        height: 160rpx;
+        border-radius: 50%;
+        overflow: hidden; // 给父级加border-radius: 50%;  必须设置overflow: hidden;
+
+        image {
+          width: 100%;
+          height: 100%;
+        }
+      }
+
+      .ip {
+        font-size: 44rpx;
+        color: #333;
+        padding: 20rpx 0 5rpx; // 上 左右 下
+      }
+
+      .address {
+        font-size: 28rpx;
+        color: #aaa;
+      }
+    }
+
+    .section {
+      width: 690rpx;
+      margin: 50rpx auto;
+      border: 1px solid #eee;
+      border-radius: 10rpx;
+      box-shadow: 0 0 30rpx rgba(0, 0, 0, 0.05);
+
+      .list {
+        .row {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 0 30rpx;
+          height: 100rpx;
+          border-bottom: 1px solid #eee;
+          &:last-child {
+            border-bottom: 0;
+          }
+          .left{
+            display: flex;
+            align-items: center;
+            .text{
+              padding-left: 20rpx;
+              color: #666;
+            }
+          }
+          .right{
+            display: flex;
+            align-items: center;
+            .text{
+              font-size: 28rpx;
+              color: #aaa;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
+```
+![](README_files/7.jpg)
--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,73 +1,121 @@
 <template>
-	<view class="userLayout">
-		<view class="userInfo">
-			<view class="avatar">
-				<image src="../../static/images/xxmLogo.png" mode=""></image>
-			</view>
-			<view class="ip">100.100.100.011</view>
-			<view class="address">来自于：山东</view>
+  <view class="userLayout">
+    <view class="userInfo">
+      <view class="avatar">
+        <image src="../../static/images/xxmLogo.png" mode=""></image>
+      </view>
+      <view class="ip">100.100.100.011</view>
+      <view class="address">来自于：山东</view>
 
-		</view>
-		<view class="section">
-			<view class="list">
-				<view class="row">
-					<view class="left">
-						<uni-icons type="download-filled" size="20"></uni-icons>
-						<view class="text">我的下载</view>
-					</view>
-					<view class="right">
-						<view class="text">{{userinfo.downloadSize}}</view>
-						<uni-icons type="right" size="15" color="#aaa"></uni-icons>
-					</view>
-				</view>
+    </view>
+    <view class="section">
+      <view class="list">
+        <view class="row" v-for="itme in 3">
+          <view class="left">
+            <uni-icons type="download-filled" size="20" color="#28b389"></uni-icons>
+            <view class="text">我的下载</view>
+          </view>
+          <view class="right">
+            <view class="text">66</view>
+            <uni-icons type="right" size="15" color="#aaa"></uni-icons>
+          </view>
+        </view>
 
+      </view>
+    </view>
+    <view class="section">
+      <view class="list">
+        <view class="row" v-for="itme in 2">
+          <view class="left">
+            <uni-icons type="download-filled" size="20" color="#28b389"></uni-icons>
+            <view class="text">我的下载</view>
+          </view>
+          <view class="right">
+            <view class="text">66</view>
+            <uni-icons type="right" size="15" color="#aaa"></uni-icons>
+          </view>
+        </view>
 
-
-			</view>
-		</view>
-		<view class="section">
-
-		</view>
-	</view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script setup>
-	import {
-		ref
-	} from "vue";
+  import {
+    ref
+  } from "vue";
 </script>
 
 <style lang="scss" scoped>
-	.userLayout {
-		.userInfo {
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			align-items: center;
-			padding: 50rpx 0;
+  .userLayout {
+    .userInfo {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 50rpx 0;
 
-			.avatar {
-				width: 160rpx;
-				height: 160rpx;
-				border-radius: 50%;
-				overflow: hidden; // 给父级加border-radius: 50%;  必须设置overflow: hidden;
+      .avatar {
+        width: 160rpx;
+        height: 160rpx;
+        border-radius: 50%;
+        overflow: hidden; // 给父级加border-radius: 50%;  必须设置overflow: hidden;
 
-				image {
-					width: 100%;
-					height: 100%;
-				}
-			}
+        image {
+          width: 100%;
+          height: 100%;
+        }
+      }
 
-			.ip {
-				font-size: 44rpx;
-				color: #333;
-				padding: 20rpx 0 5rpx; // 上 左右 下
-			}
+      .ip {
+        font-size: 44rpx;
+        color: #333;
+        padding: 20rpx 0 5rpx; // 上 左右 下
+      }
 
-			.address {
-				font-size: 28rpx;
-				color: #aaa;
-			}
-		}
-	}
+      .address {
+        font-size: 28rpx;
+        color: #aaa;
+      }
+    }
+
+    .section {
+      width: 690rpx;
+      margin: 50rpx auto;
+      border: 1px solid #eee;
+      border-radius: 10rpx;
+      box-shadow: 0 0 30rpx rgba(0, 0, 0, 0.05);
+
+      .list {
+        .row {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 0 30rpx;
+          height: 100rpx;
+          border-bottom: 1px solid #eee;
+          &:last-child {
+            border-bottom: 0;
+          }
+          .left{
+            display: flex;
+            align-items: center;
+            .text{
+              padding-left: 20rpx;
+              color: #666;
+            }
+          }
+          .right{
+            display: flex;
+            align-items: center;
+            .text{
+              font-size: 28rpx;
+              color: #aaa;
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
